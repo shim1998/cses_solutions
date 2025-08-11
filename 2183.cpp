751f2e0202acf132d1c0f267f7ff52091b104d29
shim1998 <98shim@gmail.com>
2025-08-12 01:28:19 +0530

    unsolved 1

--- 2183.cpp
+++ 2183.cpp
@@ -1,22 +1,14 @@
-#include<bits/stdc++.h>
-using namespace std;
-void solve(){
-	int n,m;
-	cin>>n>>m;
-	vector<int>a(n);
-	for(int i=0;i<n;i++) cin>>a[i];
-	sort(a.begin(),a.end());
-	long long int val=1,ans;
-	for(int i: a){
-		if(i>val){
-			ans=val;
-			break;
-		}
-		val+=i;
-	}
-	cout<<ans;
-}
+// #include<bits/stdc++.h>
+// using namespace std;
+// void solve(){
+// 	int n,m;
+// 	cin>>n>>m;
+// 	vector< vector< pair<int,int> > >adj(n);
+// 	for(int i=0;i<m;i++){
+
+// 	}
+// }
  
-int main(){
-	solve();
-}
+// int main(){
+// 	solve();
+// }
